901d30f2cbdca2f5ee39a688f183ca59a143026b
milan <milan.dima@vives.be>
2022-11-20 19:23:29 +0100

    path get_post.js aangepast in first.php

--- first.php
+++ first.php
@@ -6,7 +6,7 @@
 <link rel="stylesheet" href="style.css"/>
 <!-- Include JavaScript File Here-->
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script type="text/javascript" src="js/get_post.js"></script>
+<script type="text/javascript" src="get_post.js"></script>
 </head>
 <body>
 <div class="container">
@@ -14,8 +14,10 @@
 <form method="" action="first.php" id="form">
 <h2>PHP GET and POST Method Example</h2>
 <label>Select Form Method :</label>
-<span><input type="radio" name="method" value="post"> POST
-<input type="radio" name="method" value="get"> GET </span>
+<span>
+    <input type="radio" name="method" value="post"> POST
+    <input type="radio" name="method" value="get"> GET 
+</span>
 <label>First Name :</label>
 <input type="text" name="fname" id="fname" />
 <label>Last Name :</label>
